f1a86f3cef323d56365447ac65882e38474ca001
李国建 <guojian.li@BKJK.COM>
2018-08-20 12:04:34 +0800

    init checkin

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@
 import os
 from setuptools import setup
 
+import codecs
+
+def read(fname):
+
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name='databot',
       description='data flow programming framework with asyncio'
                   ' ',
-      long_description='data flow programming framework with asyncio',
+      long_description=read("README.rst"),
       version='0.1.0',
       url='https://github.com/guojianli/databot',
       author='Guojian Li',
       author_email='guojianlee@gmail.com',
       license='BSD',
       classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: Apache Software License',
+          'Development Status :: 1 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
           'Programming Language :: Python :: 3'
       ],
       packages=['databot'],
