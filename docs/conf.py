00336210b18da9aa900ec0963cc2596eb37b268d
gj <guojian.li@BKJK.COM>
2018-09-08 19:36:20 +0800

    modify rst doc

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'databotdoc'
+htmlhelp_basename = 'botflowdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'databot.tex', 'databot Documentation',
+    (master_doc, 'botflow.tex', 'botflow Documentation',
      'kkyon', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'databot', 'databot Documentation',
+    (master_doc, 'botflow', 'botflow Documentation',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'databot', 'databot Documentation',
-     author, 'databot', 'One line description of project.',
+    (master_doc, 'botflow', 'botflow Documentation',
+     author, 'botflow', 'One line description of project.',
      'Miscellaneous'),
 ]
